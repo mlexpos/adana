f18e49c016fa420fc678ca17d64808461eca384a
damien.ferbach <damien.ferbach@cn-f004.server.mila.quebec>
2025-09-17 23:44:23 -0400

    dana.sh file

---
+++ scripts/124m/dana.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+uv run torchrun --standalone --nproc_per_node=1 ./src/main.py --config_format base --model llama \
+    --distributed_backend nccl --compile \
+    --n_embd 768 --n_head 12 --n_layer 12 \
+    --batch_size 64 --sequence_length 512 --acc_steps 4 \
+    --dataset fineweb --iterations 64000 \
+    --dropout 0.0  --grad_clip 0.5 --seed 0 \
+    --opt dana --lr 1e-3 --delta 8 --kappa 0.75 \
+    --scheduler cos --warmup_steps 2000 \
+    --weight_decay 0.1 \
+    --beta1 0.9 --beta2 0.999 \
+    --wandb --wandb_project $WANDB_PROJECT  --wandb_entity $WANDB_ENTITY \
+    --eval_interval 115 --log_interval 50 --latest_ckpt_interval 1000 \
+    
+
+
