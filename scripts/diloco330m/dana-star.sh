21eedbfbb86062d7c0001763738645b12d298f5b
Courtney Paquette <cykempton@gmail.com>
2025-09-28 11:23:31 -0400

    Update training parameters in adamw.sh and dana-star.sh
    - Increased the number of iterations from 100708 to 129312 for extended model training duration.
    - Adjusted warmup steps from 2000 to 2586 to enhance learning rate scheduling.

--- scripts/diloco330m/dana-star.sh
+++ scripts/diloco330m/dana-star.sh
@@ -5,10 +5,10 @@ torchrun --standalone --nproc_per_node=1 ./src/main.py --config_format base --mo
     --n_embd 1280 --qkv_dim 64 --n_head 20 --n_layer 15 \
     --mlp_hidden_dim 5120 \
     --batch_size 32 --sequence_length 2048 --acc_steps 1 \
-    --dataset fineweb --iterations 100708 \
+    --dataset fineweb --iterations 129312 \
     --dropout 0.0 --grad_clip 2.5 --seed 0 \
     --opt dana-star --lr 5e-4 --delta 8 --kappa 0.75 --clipsnr 1.6 \
-    --scheduler cos --warmup_steps 2000 \
+    --scheduler cos --warmup_steps 2586 \
     --weight_decay 0.001 --wd_decaying --wd_ts 100 \
     --beta1 0.9 --beta2 0.999 \
     --wandb --wandb_project $WANDB_PROJECT  --wandb_entity $WANDB_ENTITY \
