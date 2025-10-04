53dd91bbf72948468f5f0eea1e5446390908199d
Courtney Paquette <cykempton@gmail.com>
2025-10-03 22:51:13 -0400

    Update training script parameters for consistency and optimization
    - Reduced the number of iterations from 90000 to 43024 in `narval-cypaq-ademamix-nozloss.sh` to align with recent changes.
    - Increased learning rate from 6e-4 to 1e-3 and weight decay from 0.0741 to 1e-3 for improved model performance.
    - Adjusted warmup steps for `adema_beta3` and `adema_alpha` to match the new iteration count.

--- scripts/diloco90m/narval-cypaq-ademamix-nozloss.sh
+++ scripts/diloco90m/narval-cypaq-ademamix-nozloss.sh
@@ -40,16 +40,14 @@ torchrun --standalone --nproc_per_node=1 ./src/main.py --config_format base --mo
     --mlp_hidden_dim 3072 \
     --batch_size 32 --sequence_length 2048 --acc_steps 1 \
     --datasets_dir "$DATASETS_DIR" --dataset fineweb_100 \
-    --iterations 90000 \
+    --iterations 43024 \
     --dropout 0.0 --warmup_steps 860 --grad_clip 0.5 --seed 0 \
     --z_loss_coeff 0.0 \
-    --opt ademamix --lr 6e-4 --weight_decay 0.0741 \
+    --opt ademamix --lr 1e-3 --weight_decay 1e-3 \
     --beta1 0.9 --beta2 0.999 \
     --scheduler cos_inf --cos_inf_steps 0 --div_factor 1e2 --final_div_factor 1e-1 \
     --delta 8 --kappa 0.75 --gamma_3_factor 1.0 \
-    --adema_beta3_warmup 90000 --adema_alpha_warmup 90000 \
+    --adema_beta3_warmup 43024 --adema_alpha_warmup 43024 \
     --wandb --wandb_project $WANDB_PROJECT  --wandb_entity $WANDB_ENTITY \
     --eval_interval 115
-    #    --iterations 43024 \
-    #--adema_beta3_warmup 43024 --adema_alpha_warmup 43024 \
     
