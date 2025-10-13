ef42feb85826a104070ebdc8064caa0438a0e714
Elliot Paquette <elliot.paquette@gmail.com>
2025-10-12 22:21:39 -0400

    Relaunched formula 5

--- src/optim/dana_star_mk4.py
+++ src/optim/dana_star_mk4.py
@@ -217,11 +217,12 @@ class DANA_STAR_MK4(Optimizer):
                 #g3_term = g3 * ( 0.125*torch.sign(m) * self._tau_reg(tau, step) + clipsnr * m * norm_term)
 
                 #formula 5 (CORRESPONDS to A,B -1.0/1.0/KAPPA1.0) but now attempting to stabilize it differently
-                #g3_term = g3 * self._tau_reg(tau, step)*(0.125*torch.sign(m))*(1.0 + clipsnr*torch.clamp(effective_time*((norm_term*torch.abs(m)/self._tau_reg(tau, step))**3),max=1.0)) 
-                #(This didn't appear to improve anything)
+                g3_term = g3 * self._tau_reg(tau, step)*(0.0625*torch.sign(m))*(1.0 + clipsnr*torch.clamp(effective_time*((norm_term*torch.abs(m)/self._tau_reg(tau, step))**3),max=1.0)) 
+                #(At first, this didn't appear to improve anything, but after further review, it appears that the clipsnr was too low)
+                #(So to improve behavior, I further dropped the snr constant on the 0.125 to 0.0625, and then launched a larger sweep, with larger clipsnr.)
 
                 #formula 6 (CORRESPONDS to A,B 0.5/-0.5/KAPPA0.0) Boosted with a clipped dana-star recipe.
-                g3_term = g3 * self._tau_reg(tau, step)*(0.125*torch.sign(m))*(1.0 + clipsnr*torch.clamp(effective_time**0.25*(norm_term*torch.abs(m)),max=1.0)) 
+                #g3_term = g3 * self._tau_reg(tau, step)*(0.125*torch.sign(m))*(1.0 + clipsnr*torch.clamp(effective_time**0.25*(norm_term*torch.abs(m)),max=1.0)) 
 
 
                 # Compute parameter updates using effective time for g2 and g3 scheduling
