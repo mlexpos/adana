0499107663a188857db8455532a607ea52bf2810
Elliot Paquette <elliot.paquette@gmail.com>
2025-11-18 15:49:15 -0800

    Update cluster list scripts to include job allocation information in output format for improved job tracking.

--- scripts/cluster_listqueue.sh
+++ scripts/cluster_listqueue.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-squeue -t PD -S -Q -o "%.10Q %.9P %.20j %.8u %.8T %.10M %.6D %.15b" | grep -E "(JOBID|gpu)" 
+squeue -t PD -S -Q -o "%.10Q %.9P %.10a %.20j %.8u %.8T %.10M %.6D %.15b" | grep -E "(JOBID|gpu)" 
